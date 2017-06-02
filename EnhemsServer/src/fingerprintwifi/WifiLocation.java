26001cbb9f1619d8d6355c10d9e1d83298a5ecd2
finger-numb <teotoplak95@gmail.com>
2017-06-02 09:46:43 +0200

    Implemented Gauss kernel for alocation

--- EnhemsServer/src/fingerprintwifi/WifiLocation.java
+++ EnhemsServer/src/fingerprintwifi/WifiLocation.java
@@ -1,5 +1,6 @@
 package fingerprintwifi;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -12,13 +13,89 @@ public class WifiLocation {
             "hometest/RSSFingerPrintTomislavDoma.txt"
     };
 
+    private static Map<String, Map<String, Double>> fingerprintMeanMap;
+
+    private static Double SIGMA = 0.25;
+
     public static void main(String[] args) {
-        locate(PATH_TO_FILES);
+
+        Map<String,Integer> rsssUser = new HashMap<>();
+        rsssUser.put("ec:8a:4c:92:57:10", -86);
+        rsssUser.put("84:16:f9:01:e5:ae", -77);
+        rsssUser.put("fc:b4:e6:7b:4f:f2", -84);
+        rsssUser.put("cc:03:fa:e4:a0:8b", -55);
+        rsssUser.put("78:96:82:20:df:f2", -64);
+        rsssUser.put("70:5a:9e:a8:f6:3e", -82);
+        rsssUser.put("8c:79:67:9d:63:1e", -86);
+        rsssUser.put("88:f7:c7:4f:0d:bb", -74);
+        rsssUser.put("00:18:9b:6c:c9:78", -84);
+        locate(rsssUser);
+
+
     }
 
-    public static String locate(String[] pathToFiles) {
-        Map<String, Map<String, Double>> fingerprintMeanMap = PreProcessor.meanValuesMap(pathToFiles);
-        System.out.println(fingerprintMeanMap);
+    public static String locate(Map<String,Integer> rsssUser) {
+
+        //initialize if doesnt exist
+        //TODO put in server initializer
+        fingerprintMeanMap = PreProcessor.meanValuesMap(PATH_TO_FILES);
+
+
+        Map<String, Double> likehoods = new HashMap<>();
+
+        for (Map.Entry<String, Map<String, Double>> fingerprintMeanEntry : fingerprintMeanMap.entrySet()) {
+            String room = fingerprintMeanEntry.getKey();
+            Map<String, Double> meanValuesMap = fingerprintMeanEntry.getValue();
+            likehoods.put(room, gaussKernel(rsssUser, meanValuesMap));
+        }
+
+        System.out.println(likehoods);
+
         return null;
     }
+
+    /**
+     * Method implements Gauss RBF function
+     * @param rsssUser vector of rss's from user
+     * @param rsssMean vector
+     * @return likehood value
+     */
+    private static Double gaussKernel(Map<String,Integer> rsssUser, Map<String,Double> rsssMean) {
+        Double likehood = 0.0;
+        for (Map.Entry<String, Double> rssMeanEntry : rsssMean.entrySet()) {
+            String mac = rssMeanEntry.getKey();
+            Double rssMean = rssMeanEntry.getValue();
+            Integer rssUser = rsssUser.get(mac);
+            if (rssUser == null) {
+                continue;
+            }
+            likehood +=
+                    (1 / (Math.sqrt(2 * Math.PI) * SIGMA))
+                            * Math.exp((-1) * ((Math.pow(rssMean - rssUser, 2)) / (2 * Math.pow(SIGMA, 2))));
+        }
+        return likehood;
+    }
+
+    /*
+        Tinas room
+        rsssUser.put("78:8c:54:03:14:d7", -84);
+        rsssUser.put("14:60:80:78:40:d6", -81);
+        rsssUser.put("70:5a:9e:a9:02:91", -84);
+        rsssUser.put("fc:52:8d:76:52:a2", -83);
+        rsssUser.put("fc:b4:e6:7b:4f:f2", -82);
+        rsssUser.put("cc:03:fa:e4:a0:8b", -68);
+        rsssUser.put("70:5a:9e:a8:f6:3e", -78);
+        rsssUser.put("08:95:2a:5e:c2:83", -87);
+        rsssUser.put("f4:cb:52:5b:91:b0", -84);
+        rsssUser.put("b4:a5:ef:aa:b5:be", -83);
+        rsssUser.put("2a:28:5d:40:07:24", -87);
+        rsssUser.put("88:f7:c7:4f:0d:bb", -86);
+        rsssUser.put("94:4a:0c:53:d5:d6", -82);
+        rsssUser.put("00:18:9b:6c:c9:78", -85);
+
+
+
+     */
+
+
 }
